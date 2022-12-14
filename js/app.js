caef9a42ae2449e295b6a9bf25177f51421735a4
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-12-14 17:15:05 +0100

    Add program for counter - Problem at decrement to fix

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// QUANTITY DISPLAY
+
+let ingredientsQuantities = document.getElementsByClassName('quantity');
+
+let quantitiesArray = [].slice.call(ingredientsQuantities); // Create an array from HTML collection
+
+// SET GUEST COUNTER
+
+let guestCounter = 1 // 1 by default
+let displayedGuestNumber = document.getElementById('guests--counter-number') 
+
+increment = () => {
+	if(guestCounter < 12){ //Max guests : 12
+		guestCounter ++;
+		console.log(guestCounter);
+		displayedGuestNumber.innerText = guestCounter;
+
+		quantitiesArray.forEach(element => {
+			element.innerText = (element.innerText * guestCounter);
+		});
+	}
+}
+
+decrement = () => {
+	if(guestCounter > 1){ // Min guests = 1
+		guestCounter --;
+		console.log(guestCounter);
+		displayedGuestNumber.innerText = guestCounter;
+
+		quantitiesArray.forEach(element => {
+			element.innerText = (element.innerText * guestCounter);
+		});
+	} 
+}
+
+// Prbolem at decrement
+
+// quantitiesArray.forEach(element => { // Not worked
+// 	element.innerText = element.innerText * guestCounter;
+// })
+

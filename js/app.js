95d30dd7a064d9d3129dfa11655011846b05aa02
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-12-15 11:25:03 +0100

    add import json

--- js/app.js
+++ js/app.js
@@ -1,43 +1,46 @@
+import recipe from './recipe.json' assert {type : 'json'};
+console.log(recipe);
+
 // QUANTITY DISPLAY
 
-let ingredientsQuantities = document.getElementsByClassName('quantity');
+let ingredientsQuantities = document.getElementsByClassName("quantity");
 const initialQuantities = [].slice.call(ingredientsQuantities); // Create an array from HTML collection
-let calculatedQuantities = initialQuantities;
+let calculatedQuantities = Array.from(initialQuantities);
 
 // SET GUEST COUNTER
 
-let guestCounter = 1 // 1 by default
-let displayedGuestNumber = document.getElementById('guests--counter-number') 
+let guestCounter = 1; // 1 by default
+let displayedGuestNumber = document.getElementById("guests--counter-number");
 
 increment = () => {
-	if(guestCounter < 12){ //Max guests : 12
-		guestCounter ++;
+	if (guestCounter < 12) {
+		//Max guests : 12
+		guestCounter++;
 		console.log(guestCounter);
 		displayedGuestNumber.innerText = guestCounter;
 
-// ADD initial quantity to quantity calculated (Not multiply)
-
-		calculatedQuantities.forEach(element => {
-			element.innerText = (element.innerText + // initial quantity);
-		});
+		// ADD initial quantity to quantity calculated (Not multiply)
+		calculatedQuantities.forEach((element, index) => {
+			element.innerText = parseFloat(element.innerText) + parseFloat(initialQuantities[index].innerText);
+		})
 	}
-}
+};
 
 decrement = () => {
-	if(guestCounter > 1){ // Min guests = 1
-		guestCounter --;
+	if (guestCounter > 1) {
+		// Min guests = 1
+		guestCounter--;
 		console.log(guestCounter);
 		displayedGuestNumber.innerText = guestCounter;
 
 		// quantitiesArray.forEach(element => {
 		// 	element.innerText = (element.innerText * guestCounter);
 		// });
-	} 
-}
+	}
+};
 
 // Prbolem at decrement
 
 // quantitiesArray.forEach(element => { // Not worked
 // 	element.innerText = element.innerText * guestCounter;
 // })
-

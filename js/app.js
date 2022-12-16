b99662951ccf3222798df4781b843a93ae3bdaab
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2022-12-16 15:47:07 +0100

    add fetch API

--- js/app.js
+++ js/app.js
@@ -1,76 +1,87 @@
-// IMPORT RECIPE DATA
-import recipe from "./recipe.json" assert { type: "json" };
+// // IMPORT RECIPE DATA
+// import recipe from "./recipe.json" assert { type: "json" };
 
-//DISPLAY RECIPE TITLE
-const recipeTitle = document.querySelector(".title h1");
-recipeTitle.innerText = recipe.title;
+// FETCH API APPROACH
+fetch("/js/recipe.json")
+	.then((response) => response.json())
+	.then((recipe) => {
+		console.log(recipe);
 
-// DISPLAY INGREDIENTS
-const table = document.querySelector("table");
+		//DISPLAY RECIPE TITLE
+		const recipeTitle = document.querySelector(".title h1");
+		recipeTitle.innerText = recipe.title;
 
-recipe.ingredients.forEach((element) => {
-	const newRow = table.insertRow();
+		// DISPLAY INGREDIENTS
+		const table = document.querySelector("table");
 
-	const newIngredient = newRow.insertCell();
-	newIngredient.innerText = element.name;
-	newIngredient.classList.add("ingredient");
+		recipe.ingredients.forEach((element) => {
+			const newRow = table.insertRow();
 
-	const newQty = newRow.insertCell();
-	newQty.innerText = element.quantity;
-	newQty.classList.add("quantity");
+			const newIngredient = newRow.insertCell();
+			newIngredient.innerText = element.name;
+			newIngredient.classList.add("ingredient");
 
-	const newUnit = newRow.insertCell();
-	newUnit.innerText = element.unit;
-	newUnit.classList.add("unit");
-});
+			const newQty = newRow.insertCell();
+			newQty.innerText = element.quantity;
+			newQty.classList.add("quantity");
 
-// DISPLAY STEPS
-const stepList = document.querySelector(".step-list");
-recipe.steps.forEach((element) => {
-	let newStep = stepList.appendChild(document.createElement("li"));
-	newStep.textContent = element.step;
-});
+			const newUnit = newRow.insertCell();
+			newUnit.innerText = element.unit;
+			newUnit.classList.add("unit");
+		});
 
-// SET GUEST COUNTER
-let guestCounter = 1; // 1 by default
-const displayedGuestNumber = document.getElementById("guests--counter-number");
-const btnPlus = document.getElementById("plus");
-const btnMinus = document.getElementById("minus");
+		// DISPLAY STEPS
+		const stepList = document.querySelector(".step-list");
+		recipe.steps.forEach((element) => {
+			let newStep = stepList.appendChild(document.createElement("li"));
+			newStep.textContent = element.step;
+		});
 
-btnPlus.addEventListener("click", increment);
-btnMinus.addEventListener("click", decrement);
+		// SET GUEST COUNTER
+		let guestCounter = 1; // 1 by default
+		const displayedGuestNumber = document.getElementById(
+			"guests--counter-number"
+		);
+		const btnPlus = document.getElementById("plus");
+		const btnMinus = document.getElementById("minus");
 
-//--------FUNCTIONS INCREMENT & DECREMENT--------
-let displayedQuantities = document.getElementsByClassName("quantity");
+		btnPlus.addEventListener("click", increment);
+		btnMinus.addEventListener("click", decrement);
 
-function increment() {
-	if (guestCounter < 12) {
-		//Max guests : 12
-		guestCounter++;
-		displayedGuestNumber.innerText = guestCounter;
+		//--------FUNCTIONS INCREMENT & DECREMENT--------
+		let displayedQuantities = document.getElementsByClassName("quantity");
 
-		// Increment quantities (Only if quantity is specified - avoid a NaN)
-		for (let i = 0; i < displayedQuantities.length; i++) {
-			if (recipe.ingredients[i].quantity != "") {
-				displayedQuantities[i].innerText =
-					displayedQuantities[i].innerText * 1 + recipe.ingredients[i].quantity;
+		function increment() {
+			if (guestCounter < 12) {
+				//Max guests : 12
+				guestCounter++;
+				displayedGuestNumber.innerText = guestCounter;
+
+				// Increment quantities (Only if quantity is specified - avoid a NaN)
+				for (let i = 0; i < displayedQuantities.length; i++) {
+					if (recipe.ingredients[i].quantity != "") {
+						displayedQuantities[i].innerText =
+							displayedQuantities[i].innerText * 1 +
+							recipe.ingredients[i].quantity;
+					}
+				}
 			}
 		}
-	}
-}
 
-function decrement() {
-	if (guestCounter > 1) {
-		// Min guests = 1
-		guestCounter--;
-		displayedGuestNumber.innerText = guestCounter;
+		function decrement() {
+			if (guestCounter > 1) {
+				// Min guests = 1
+				guestCounter--;
+				displayedGuestNumber.innerText = guestCounter;
 
-		// Decrement quantities (Only if quantity is specified - avoid a NaN)
-		for (let i = 0; i < displayedQuantities.length; i++) {
-			if (recipe.ingredients[i].quantity != "") {
-				displayedQuantities[i].innerText =
-					displayedQuantities[i].innerText * 1 - recipe.ingredients[i].quantity;
+				// Decrement quantities (Only if quantity is specified - avoid a NaN)
+				for (let i = 0; i < displayedQuantities.length; i++) {
+					if (recipe.ingredients[i].quantity != "") {
+						displayedQuantities[i].innerText =
+							displayedQuantities[i].innerText * 1 -
+							recipe.ingredients[i].quantity;
+					}
+				}
 			}
 		}
-	}
-}
+	});
